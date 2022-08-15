77a232f0c53a17189521fb86cd3ab2a18487ea9a
devumairazmat <umairazmat8260@gmail.com>
2022-08-16 01:51:16 +0500

    Add or Edit Function

---
+++ src/Utils.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+import db from "./firebase";
+
+  // Btn Function Add Color To Collection
+export const addNew = async () => {
+    const collectionRef = collection(db,"color");
+    const name = prompt("Add new Color");
+    const value = prompt("Add value of Color");
+    const payload ={ name ,value}
+    const docRef = await addDoc(collectionRef,payload);
+    console.log("The new Color Id is:",docRef.id)
+  }
+
+  // Edit Color Func Added to Collection
+  export const editColor = async (id) => {
+    const name = prompt("Add new Color");
+    const value = prompt("Add value of Color");
+    const docRef = doc(db,"color",id);
+    const payload ={ name ,value}
+    setDoc (docRef, payload)
+  }
