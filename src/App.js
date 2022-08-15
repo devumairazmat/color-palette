3ca3997f2f9e75da2dab9ccbbc5aed95e394e0e6
devumairazmat <umairazmat8260@gmail.com>
2022-08-16 00:45:00 +0500

    add color by clicking on btn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Button from "react-bootstrap/Button";
 import db from "./firebase";
 import { useEffect, useState } from "react";
-import { onSnapshot, collection, doc } from "firebase/firestore";
+import { onSnapshot, collection, doc ,addDoc  } from "firebase/firestore";
 
 const Dot = ({ color }) => {
   const Style = {
@@ -20,9 +20,7 @@ const Dot = ({ color }) => {
 
 function App() {
   
-  const [colors, setColors] = useState([]);
-  console.log(colors);
-  
+  const [colors, setColors] = useState([{name:"Loading...",id:"initial"}]);  
   useEffect(
     () =>
       onSnapshot(collection(db, "color"), (snapshot) => {
@@ -31,9 +29,17 @@ function App() {
     []
   );
 
+  const addNew = async () => {
+    const collectionRef = collection(db,"color");
+    const name = prompt("Add new Color");
+    const value = prompt("Add value of Color");
+    const payload ={ name ,value}
+    await addDoc(collectionRef,payload);
+  }
+
   return (
     <>
-      <Button className="m-4" variant="dark">
+      <Button className="m-4" variant="dark" onClick={addNew}>
         Add Color
       </Button>
       <ol className="m-4 p-4">
